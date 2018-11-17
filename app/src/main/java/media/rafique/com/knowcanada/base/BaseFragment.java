2c8fc8980b4d01e803a70cfa9280bd6cf91eda6e
Rafique <rafique.mujawar@tcognition.com>
2018-11-17 21:04:58 +0530

    Added Base activity and Base fragment

---
+++ app/src/main/java/media/rafique/com/knowcanada/base/BaseFragment.java
@@ -0,0 +1,53 @@
+package media.rafique.com.knowcanada.base;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+
+import media.rafique.com.knowcanada.R;
+import media.rafique.com.presenter.base.BaseView;
+import media.rafique.com.utilitymodule.constants.KnowCanadaConstants;
+import media.rafique.com.utilitymodule.utils.KCUtils;
+
+/**
+ * @author Rafique Mujawar
+ * Date 17-11-2018
+ */
+public abstract class BaseFragment extends Fragment implements BaseView {
+  /**
+   * Returns Context
+   *
+   * @return context
+   */
+  @Override
+  public Context getViewContext() {
+    if (!isAdded()) {
+      return null;
+    }
+    return getContext();
+  }
+
+  /**
+   * Method to display error when network is not available
+   */
+  @Override
+  public void showNetworkError() {
+    if (!isAdded()) {
+      return;
+    }
+    String title = getString(R.string.no_network_title);
+    String msg = getString(R.string.no_network_msg);
+    KCUtils.showAlertDialog(getViewContext(), title, msg);
+  }
+
+  /**
+   * Method to check if connected is available
+   *
+   * @return - true if network is available else false
+   */
+  @Override
+  public boolean isNetworkAvailable() {
+    return isAdded() && KCUtils.getNetworkStatus(getViewContext()) !=
+        KnowCanadaConstants
+            .NOT_CONNECTION_STATUS;
+  }
+}
