6e5e13bd383f66f4a8f0aedb3ccc3df1dcea96a9
Rafique <rafique.mujawar@tcognition.com>
2018-11-29 16:00:39 +0530

    Added Unit test cases for KCUtility methods to verify negative and positive scenario

--- utilitymodule/src/test/java/media/rafique/com/utilitymodule/utils/KCUtilsTest.java
+++ utilitymodule/src/test/java/media/rafique/com/utilitymodule/utils/KCUtilsTest.java
@@ -1,34 +1,65 @@
 package media.rafique.com.utilitymodule.utils;
 
+import android.text.TextUtils;
+
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import media.rafique.com.utilitymodule.data.HomeResponseItem;
+
+import static org.mockito.Matchers.any;
 
 /**
  * @author Rafique Mujawar
  * Date 19-11-2018
  */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(TextUtils.class)
 public class KCUtilsTest {
 
+  @Before
+  public void setup() {
+    PowerMockito.mockStatic(TextUtils.class);
+    PowerMockito.when(TextUtils.isEmpty(any(CharSequence.class))).thenAnswer(new Answer<Boolean>() {
+      @Override
+      public Boolean answer(InvocationOnMock invocation) {
+        CharSequence a = (CharSequence) invocation.getArguments()[0];
+        return !(a != null && a.length() > 0);
+      }
+    });
+  }
+
   @Test
-  public void testValidFactItem1() {
+  public void testValidFactItemWithValidTitle() {
     HomeResponseItem item = new HomeResponseItem();
     item.setTitle("Test title");
-    Assert.assertEquals(true, KCUtils.isValidFactItem(item));
+    Assert.assertTrue(KCUtils.isValidFactItem(item));
   }
 
   @Test
-  public void testValidFactItem2() {
+  public void testValidFactItemWithValidDescription() {
     HomeResponseItem item = new HomeResponseItem();
     item.setDescription("Test description");
-    Assert.assertEquals(true, KCUtils.isValidFactItem(item));
+    Assert.assertTrue(KCUtils.isValidFactItem(item));
   }
 
   @Test
-  public void testValidFactItem3() {
+  public void testValidFactItemWithValidHref() {
     HomeResponseItem item = new HomeResponseItem();
     item.setImageHref("Test image link");
-    Assert.assertEquals(true, KCUtils.isValidFactItem(item));
+    Assert.assertTrue(KCUtils.isValidFactItem(item));
+  }
+
+  @Test
+  public void testValidFactItemWithInValidData() {
+    HomeResponseItem item = new HomeResponseItem();
+    Assert.assertFalse(KCUtils.isValidFactItem(item));
   }
 }
